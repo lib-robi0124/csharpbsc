c999426aa82cb09dcdfd72c1e6d1364b2cb7a7bb
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-02-28 20:15:05 +0100

    kjh

---
+++ homework.class02/homework.class02/homework.class02/Program.cs
@@ -0,0 +1,13 @@
+﻿
+#region RealCalculator
+Console.WriteLine("Please enter a first number to calculate");
+bool userFirstInput = int.TryParse(Console.ReadLine(), out int parsedFirstNumber);
+Console.WriteLine("Please enter a second number to calculate");
+bool userSecondInput = int.TryParse(Console.ReadLine(), out int parsedSecoundNumber);
+Console.WriteLine("Please enter an operation to calculate");
+string userOperationInput = Console.ReadLine();
+
+
+
+#endregion
+
