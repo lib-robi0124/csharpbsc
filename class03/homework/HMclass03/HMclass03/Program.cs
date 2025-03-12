53a4609461771e6cff94173ca88eba55beb9f5bf
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-03-12 16:40:46 +0100

    cfd

--- class03/homework/HMclass03/HMclass03/Program.cs
+++ class03/homework/HMclass03/HMclass03/Program.cs
@@ -1,64 +1,64 @@
 ﻿
 #region Task1 == SumOfEven
 
-//bool userinput;
-//int i = 0;
-//int sum = 0;
-//int[] numbers = new int[6];
-//do
-//{
-//    Console.Write("Enter number: ");
-//    string input = Console.ReadLine();
-//    userinput = int.TryParse(input, out int parsedNumber);
-//    if (userinput)
-//    {
-//        numbers[i] = parsedNumber;
-//        i++;
-//        if (parsedNumber % 2 == 0)
-//        {
-//            sum += parsedNumber;
-//        }
-//    }
-//    else
-//    {
-//        Console.WriteLine("Invalid input! Try again!");
-//    }
-//} while (i < 6);
-//for (i = 0; i < numbers.Length; i++)
-//{
-//    Console.WriteLine($"integer no.{i + 1} is {numbers[i]}");
-//}
-//Console.WriteLine($"Sum of even numbers is {sum}");
+bool userinput;
+int i = 0;
+int sum = 0;
+int[] numbers = new int[6];
+do
+{
+    Console.Write("Enter number: ");
+    string input = Console.ReadLine();
+    userinput = int.TryParse(input, out int parsedNumber);
+    if (userinput)
+    {
+        numbers[i] = parsedNumber;
+        i++;
+        if (parsedNumber % 2 == 0)
+        {
+            sum += parsedNumber;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input! Try again!");
+    }
+} while (i < 6);
+for (i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine($"integer no.{i + 1} is {numbers[i]}");
+}
+Console.WriteLine($"Sum of even numbers is {sum}");
 
 
 
 #endregion
 
 #region Task2 == StudentGroup
-//string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
-//string[] studentsG2 = new string[] { "Janko", "Ranko", "Mile", "Pero", "Zile" };
-//Console.WriteLine("Enter student group: (e.g. 1 for G1 or 2 for G2)");
-//int group = int.Parse(Console.ReadLine());
-//if (group == 1)
-//{
-//    Console.WriteLine("The students in G1 are:");
-//    foreach (string student in studentsG1)
-//    {
-//        Console.WriteLine(student);
-//    }
-//}
-//else if (group == 2)
-//{
-//    Console.WriteLine("The students in G2 are:");
-//    foreach (string student in studentsG2)
-//    {
-//        Console.WriteLine(student);
-//    }
-//}
-//else
-//{
-//    Console.WriteLine("Invalid input! Try again!");
-//}
+string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[] { "Janko", "Ranko", "Mile", "Pero", "Zile" };
+Console.WriteLine("Enter student group: (e.g. 1 for G1 or 2 for G2)");
+int group = int.Parse(Console.ReadLine());
+if (group == 1)
+{
+    Console.WriteLine("The students in G1 are:");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (group == 2)
+{
+    Console.WriteLine("The students in G2 are:");
+    foreach (string student in studentsG2)
+    {
+        Console.WriteLine(student);
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input! Try again!");
+}
 
 #endregion
 
--- class04/qinshift.class04/qinshift.strings/Program.cs
+++ class04/qinshift.class04/qinshift.strings/Program.cs
@@ -2,50 +2,142 @@
 Console.WriteLine("== strings ==");
 Console.ResetColor();
 
-#region String Concatenation
-// normal strings
-string firstName = "John";
-string lastName = "Doe";
+//#region String Concatenation
+//// normal strings
+//string firstName = "John";
+//string lastName = "Doe";
 
-// string concatenation
-// 1) using "+" operator
-string hello1 = "Hello " + firstName + " " + lastName;
-Console.WriteLine(hello1);
+//// string concatenation
+//// 1) using "+" operator
+//string hello1 = "Hello " + firstName + " " + lastName;
+//Console.WriteLine(hello1);
 
-// 2) *using String Interpolation*
-string hello2 = $"Hello {firstName} {lastName}";
-Console.WriteLine(hello2);
+//// 2) *using String Interpolation*
+//string hello2 = $"Hello {firstName} {lastName}";
+//Console.WriteLine(hello2);
 
-// 3) using String.Format
-string hello3 = String.Format("Hello {0} {1}", firstName, lastName);
-Console.WriteLine(hello3);
+//// 3) using String.Format
+//string hello3 = String.Format("Hello {0} {1}", firstName, lastName);
+//Console.WriteLine(hello3);
 
-// 4) using StringBuilder (only for info, will be explained in detail further on..)
-//StringBuilder sb = new StringBuilder();
-//sb.Append("Hello ");
-//sb.Append(firstName);
-//sb.Append(" ");
-//sb.Append(lastName);
-//string hello4 = sb.ToString();
-//Console.WriteLine(hello4);
+//// 4) using StringBuilder (only for info, will be explained in detail further on..)
+////StringBuilder sb = new StringBuilder();
+////sb.Append("Hello ");
+////sb.Append(firstName);
+////sb.Append(" ");
+////sb.Append(lastName);
+////string hello4 = sb.ToString();
+////Console.WriteLine(hello4);
+
+//// 5) using String.Concat
+//string hello5 = String.Concat("Hello ", firstName, " ", lastName);
+//Console.WriteLine(hello5);
+
+
+//#endregion
+
+
+//#region String Formatting
+
+//// 1) Currency
+//string currency = string.Format("Total: {0:C}", 123.45);
+//Console.WriteLine(currency);
+
+//// *Percent formatting*
+//string percent = string.Format("Percent: {0:P}", 0.5);
+//Console.WriteLine(percent);
+//Console.WriteLine(string.Format("Percent: {0:P0}", 0.5));
+
+//// *Custom formatting*
+//string customPhoneNumberFormat = string.Format("Phone: {0:0##-###-###}", 070358459);
+//Console.WriteLine(customPhoneNumberFormat);
+
+//int number = 123456789;
+//string customNumberFormat = string.Format("Number format: {0:#,##0}", number);
+//Console.WriteLine(customNumberFormat);
+
+//// *Alignment text formating*
+//string customAlignment = string.Format("| {0,10} | {1,20} |", "Id", "Order");
+//Console.WriteLine(customAlignment);
+
+//#endregion
+
+//#region escaping reserved characters
+//// Reserved Characters in C#: \, \n, \t, \uXXXX, ", \a, etc...
+////string escape1 = "Check your c:\ drive"; // error
+////string escape2 = "We will have "fair" elections"; // error
+////string escape3 = "The \n sign means: new line";
+////Console.WriteLine(escape3);
+
+//// 1) Escaping with "\" (backslash) 
+//string escape1 = "Check your c:\\ drive";
+//string escape2 = "We will have \"fair\" elections";
+//string escape3 = "The \\n sign means: new line";
+
+//// 2) Escaping a whole string with @
+//escape1 = @"Check your c:\ drive";
+//escape2 = @"We will have ""fair"" elections";
+//escape3 = @"The \n sign means: new line";
+//Console.WriteLine(escape1);
+//Console.WriteLine(escape2);
+//Console.WriteLine(escape3);
+
+
+//#endregion
+
+//#region using ToString
+
+//Console.WriteLine("\n\n============== ToString() ==============\n");
+//int someNumber = 123456789;
+//bool isTrue = false;
+
+//// => Convert other datatypes to string
+//string someNumberString = someNumber.ToString();
+//string isTrueString = isTrue.ToString();
+
+//Console.WriteLine($"this is someNumberString {someNumberString}");
+//Console.WriteLine($"this is isTrueString {isTrueString}");
+
+//// => Using ToString for formatting 
+//string formatedString = someNumber.ToString("#,##0");
+//Console.WriteLine($"this is formatedString {formatedString}");
+
+
+//#endregion
+
+#region String Methods
+string ourString = "   We are learning C#. It is FUN and EASY. Okay maybe just FUN.    ";
+Console.WriteLine(ourString);
+
+// 1) upper and lower case
+Console.WriteLine(ourString.ToUpper());
+Console.WriteLine(ourString.ToLower());
+
+// 2) Trim
+// => It trims the white spaces at the front and back of the string
+Console.WriteLine(ourString.Trim());
+
+// 3) Length (property)
+Console.WriteLine(ourString.Length);
+
+// 4) Split
+// => Splits the string and makes an array of strings (splits on specified character/delimeter )
+string[] splittedString = ourString.Split('.');
+Console.WriteLine("SPLITTED");
+Console.WriteLine(splittedString[0]);
+Console.WriteLine(splittedString[1]);
+Console.WriteLine(splittedString[2]);
+Console.WriteLine(splittedString[3]);
+
+// 5) join
+// => Concatenates the members of a collection (array), using the specified separator between each member
+string joinedString = string.Join(".(PERIOD)", splittedString);
+Console.WriteLine(joinedString);
+
+
 
 
 #endregion
 
-
-#region String Formatting
-
-// 1) Currency
-string currency = string.Format("Total: {0:C}", 123.45);
-Console.WriteLine(currency);
-
-// *Percent formatting*
-string percent = string.Format("Percent: {0:P}", 0.5);
-Console.WriteLine(percent);
-Console.WriteLine(string.Format("Percent: {0:P0}", 0.5));
-
-
-
-#endregion
 
 Console.ReadLine();
