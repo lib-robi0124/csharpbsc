66882b1d1c36cfda769342d360114a2660c91adc
lib-robi0124 <robert.ristovski@gmail.com>
2025-03-13 21:17:14 +0100

    jghj

---
+++ class05/qinshift.class05/qinshift.classesandobjects/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace qinshift.classesandobjects.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            SSN = GenerateSSN();
+        }
+
+        public Person(string fName, string lName, int age)
+        {
+            FirstName = fName;
+            Lastname = lName;
+            Age = age;
+            SSN = GenerateSSN();
+        }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        private long SSN { get; set; }
+        private long GenerateSSN()
+        {
+            return new Random().Next(100000, 999999);
+        }
+        public void Talk(string text)
+        {
+            Console.WriteLine($"the human named {FirstName} is saying {text}!!");
+        }
+    }
+}
---
+++ class05/qinshift.class05/qinshift.classesandobjects/Program.cs
@@ -0,0 +1,30 @@
+﻿using qinshift.classesandobjects.Models;
+#region simple class user
+
+
+//User user = new User();
+//user.UserName = "robertr";
+//user.Password = "12345";
+//Console.WriteLine(user.UserName);
+
+#endregion
+
+
+#region Classes and Objects
+
+Person bob = new Person();
+bob.FirstName = "Bob";
+bob.Lastname = "Bobsky";
+bob.Age = 30;
+// bob.SSN is private
+bob.Talk("hey there! we are learning classes and Objects!");
+
+Person jill = new Person("jill", "Wayne", 35);
+jill.Talk("Hi there!!!");
+//Console.WriteLine(jill.GetSSN);
+
+
+#endregion
+
+Console.ReadLine();
+
