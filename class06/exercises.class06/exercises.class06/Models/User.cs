3b703449cd1d0003a1f54cd5b8f63244b847dfd6
lib-robi0124 <robert.ristovski@gmail.com>
2025-03-14 21:42:03 +0100

    wdv

---
+++ class06/exercises.class06/exercises.class06/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+
+namespace exercises.class06.Models
+{
+    public class User
+    {
+        public User()
+        {
+
+        }
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string[] Message { get; set; }
+
+
+      public User(int iD, string username, string password, string[] message)
+        {
+            ID = iD;
+            Username = username;
+            Password = password;
+            Message = message;
+        }
+    }
+}
+
+
--- class06/exercises.class06/exercises.class06/Program.cs
+++ class06/exercises.class06/exercises.class06/Program.cs
@@ -1,2 +1,121 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using exercises.class06.Models;
+
+#region Task1
+
+
+//string continueExecute = string.Empty;
+//while (continueExecute.ToLower() != "n")
+//{
+//    Console.WriteLine("Please enter a number: ");
+
+//    if (double.TryParse(Console.ReadLine(), out double userInput))
+//    {
+//        if (userInput > 0)
+//        {
+//            Console.WriteLine("Positive");
+//        }
+//        else
+//        {
+//            Console.WriteLine("Negative");
+//        }
+
+//        if (userInput % 1 == 0)
+//        {
+//            Console.WriteLine("integer");
+//            if (userInput % 2 == 0)
+//            {
+//                Console.WriteLine("Even");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Odd");
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("decimal");
+//            userInput *= 10;
+//            if (userInput % 2 == 0)
+//            {
+//                Console.WriteLine("Even");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Odd");
+//            }
+//        }
+//    }
+//    Console.WriteLine(userInput);
+//    Console.WriteLine("Do you want to continue? Y / N");
+//    continueExecute = Console.ReadLine();
+//    Console.Clear();
+//}
+
+
+//Console.ReadLine();
+
+
+
+
+
+#endregion
+
+#region Task2
+
+User[] users = new User[]
+{
+    new User(20975, "robertr", "Sara1234", new string[] { "Hello Robert", "how are you?" }),
+    new User(20621, "rtoska", "1234Sara", new string[] { "Hello Toska", "what's up?" }),
+    new User(10275, "rsara", "123456", new string[] { "Hello Sara", "where are you?" }),
+};
+
+Console.Write("Enter your Username: ");
+string username = Console.ReadLine();
+Console.Write("Enter your Password: ");
+string password = Console.ReadLine();
+Console.WriteLine("Enter your ID:");
+//int userInput = Convert.ToInt32(Console.ReadLine());
+int userInput;
+
+if (!int.TryParse(Console.ReadLine(), out userInput))
+{
+    Console.WriteLine("Invalid ID format.");
+    return;
+}
+
+// Check if the user exists
+//User existingUser = users.Find(user => user.ID == userInput);
+
+
+foreach (var user in users)
+{
+    if (user != null && user.Username == username && user.Password == password)
+    {
+        Console.WriteLine($"Welcome {user.Username}. Here are your messages:");
+        foreach (var msg in user.Message)
+        {
+            Console.WriteLine(msg);
+        }
+        return;
+    }
+    else if(user.ID != userInput ) 
+        {
+        Console.WriteLine("User ID not found. Adding new user...");
+        users.Add(new User(userInput, username, password, new string[] { "No messages yet." }));
+    }
+    else
+    {
+        Console.WriteLine("Invalid username or password!");
+
+    }
+}
+    
+
+
+
+
+
+
+
+
+#endregion
