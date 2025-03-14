bb3e7fea5a264c6dcc14482ba0c17c56201d793e
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-03-14 09:18:32 +0100

    acf

---
+++ class04/qinshift.class04/hm.class04/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+//The method will have one input parameter, your birthday date
+//The method should return your age
+//Show the age of a user after he inputs a date
+//Note: take into consideration if the birthday is today, after or before today
+
+#region AgeCalculator
+Console.WriteLine("Please enter your birthday: (format: dd.MM.yyyy)");
+DateTime birthdayDate = DateTime.Parse(Console.ReadLine());
+int birthYear = birthdayDate.Year;
+Console.WriteLine($"Birthday by date only: {birthdayDate:dd.MM.yyyy}");
+Console.WriteLine($"Input year: {birthYear}");
+
+DateTime currentDateAndTime = DateTime.Now;
+int yearToday = currentDateAndTime.Year;
+Console.WriteLine($"Today's year is: {yearToday}");
+
+// Calculate age
+int age = yearToday - birthYear;
+
+// Check if birthday has passed this year
+DateTime birthdayThisYear = new DateTime(yearToday, birthdayDate.Month, birthdayDate.Day);
+
+if (birthdayThisYear > currentDateAndTime)
+{
+    age--; // If birthday hasn't occurred yet this year, subtract 1
+}
+
+if (birthdayThisYear == currentDateAndTime.Date)
+{
+    Console.WriteLine("Happy Birthday!");
+}
+
+Console.WriteLine($"You are {age} years old");
+
+#endregion
+
+
+Console.ReadLine();
--- class04/qinshift.class04/qinshift.strings/Program.cs
+++ class04/qinshift.class04/qinshift.strings/Program.cs
@@ -2,108 +2,108 @@
 Console.WriteLine("== strings ==");
 Console.ResetColor();
 
-//#region String Concatenation
-//// normal strings
-//string firstName = "John";
-//string lastName = "Doe";
+#region String Concatenation
+// normal strings
+string firstName = "John";
+string lastName = "Doe";
 
-//// string concatenation
-//// 1) using "+" operator
-//string hello1 = "Hello " + firstName + " " + lastName;
-//Console.WriteLine(hello1);
+// string concatenation
+// 1) using "+" operator
+string hello1 = "Hello " + firstName + " " + lastName;
+Console.WriteLine(hello1);
 
-//// 2) *using String Interpolation*
-//string hello2 = $"Hello {firstName} {lastName}";
-//Console.WriteLine(hello2);
+// 2) *using String Interpolation*
+string hello2 = $"Hello {firstName} {lastName}";
+Console.WriteLine(hello2);
 
-//// 3) using String.Format
-//string hello3 = String.Format("Hello {0} {1}", firstName, lastName);
-//Console.WriteLine(hello3);
+// 3) using String.Format
+string hello3 = String.Format("Hello {0} {1}", firstName, lastName);
+Console.WriteLine(hello3);
 
-//// 4) using StringBuilder (only for info, will be explained in detail further on..)
-////StringBuilder sb = new StringBuilder();
-////sb.Append("Hello ");
-////sb.Append(firstName);
-////sb.Append(" ");
-////sb.Append(lastName);
-////string hello4 = sb.ToString();
-////Console.WriteLine(hello4);
+// 4) using StringBuilder (only for info, will be explained in detail further on..)
+//StringBuilder sb = new StringBuilder();
+//sb.Append("Hello ");
+//sb.Append(firstName);
+//sb.Append(" ");
+//sb.Append(lastName);
+//string hello4 = sb.ToString();
+//Console.WriteLine(hello4);
 
-//// 5) using String.Concat
-//string hello5 = String.Concat("Hello ", firstName, " ", lastName);
-//Console.WriteLine(hello5);
-
-
-//#endregion
+// 5) using String.Concat
+string hello5 = String.Concat("Hello ", firstName, " ", lastName);
+Console.WriteLine(hello5);
 
 
-//#region String Formatting
+#endregion
 
-//// 1) Currency
-//string currency = string.Format("Total: {0:C}", 123.45);
-//Console.WriteLine(currency);
 
-//// *Percent formatting*
-//string percent = string.Format("Percent: {0:P}", 0.5);
-//Console.WriteLine(percent);
-//Console.WriteLine(string.Format("Percent: {0:P0}", 0.5));
+#region String Formatting
 
-//// *Custom formatting*
-//string customPhoneNumberFormat = string.Format("Phone: {0:0##-###-###}", 070358459);
-//Console.WriteLine(customPhoneNumberFormat);
+// 1) Currency
+string currency = string.Format("Total: {0:C}", 123.45);
+Console.WriteLine(currency);
 
-//int number = 123456789;
-//string customNumberFormat = string.Format("Number format: {0:#,##0}", number);
-//Console.WriteLine(customNumberFormat);
+// *Percent formatting*
+string percent = string.Format("Percent: {0:P}", 0.5);
+Console.WriteLine(percent);
+Console.WriteLine(string.Format("Percent: {0:P0}", 0.5));
 
-//// *Alignment text formating*
-//string customAlignment = string.Format("| {0,10} | {1,20} |", "Id", "Order");
-//Console.WriteLine(customAlignment);
+// *Custom formatting*
+string customPhoneNumberFormat = string.Format("Phone: {0:0##-###-###}", 070358459);
+Console.WriteLine(customPhoneNumberFormat);
 
-//#endregion
+int number = 123456789;
+string customNumberFormat = string.Format("Number format: {0:#,##0}", number);
+Console.WriteLine(customNumberFormat);
 
-//#region escaping reserved characters
-//// Reserved Characters in C#: \, \n, \t, \uXXXX, ", \a, etc...
-////string escape1 = "Check your c:\ drive"; // error
-////string escape2 = "We will have "fair" elections"; // error
-////string escape3 = "The \n sign means: new line";
-////Console.WriteLine(escape3);
+// *Alignment text formating*
+string customAlignment = string.Format("| {0,10} | {1,20} |", "Id", "Order");
+Console.WriteLine(customAlignment);
 
-//// 1) Escaping with "\" (backslash) 
-//string escape1 = "Check your c:\\ drive";
-//string escape2 = "We will have \"fair\" elections";
-//string escape3 = "The \\n sign means: new line";
+#endregion
 
-//// 2) Escaping a whole string with @
-//escape1 = @"Check your c:\ drive";
-//escape2 = @"We will have ""fair"" elections";
-//escape3 = @"The \n sign means: new line";
-//Console.WriteLine(escape1);
-//Console.WriteLine(escape2);
+#region escaping reserved characters
+// Reserved Characters in C#: \, \n, \t, \uXXXX, ", \a, etc...
+//string escape1 = "Check your c:\ drive"; // error
+//string escape2 = "We will have "fair" elections"; // error
+//string escape3 = "The \n sign means: new line";
 //Console.WriteLine(escape3);
 
+// 1) Escaping with "\" (backslash) 
+string escape1 = "Check your c:\\ drive";
+string escape2 = "We will have \"fair\" elections";
+string escape3 = "The \\n sign means: new line";
 
-//#endregion
-
-//#region using ToString
-
-//Console.WriteLine("\n\n============== ToString() ==============\n");
-//int someNumber = 123456789;
-//bool isTrue = false;
-
-//// => Convert other datatypes to string
-//string someNumberString = someNumber.ToString();
-//string isTrueString = isTrue.ToString();
-
-//Console.WriteLine($"this is someNumberString {someNumberString}");
-//Console.WriteLine($"this is isTrueString {isTrueString}");
-
-//// => Using ToString for formatting 
-//string formatedString = someNumber.ToString("#,##0");
-//Console.WriteLine($"this is formatedString {formatedString}");
+// 2) Escaping a whole string with @
+escape1 = @"Check your c:\ drive";
+escape2 = @"We will have ""fair"" elections";
+escape3 = @"The \n sign means: new line";
+Console.WriteLine(escape1);
+Console.WriteLine(escape2);
+Console.WriteLine(escape3);
 
 
-//#endregion
+#endregion
+
+#region using ToString
+
+Console.WriteLine("\n\n============== ToString() ==============\n");
+int someNumber = 123456789;
+bool isTrue = false;
+
+// => Convert other datatypes to string
+string someNumberString = someNumber.ToString();
+string isTrueString = isTrue.ToString();
+
+Console.WriteLine($"this is someNumberString {someNumberString}");
+Console.WriteLine($"this is isTrueString {isTrueString}");
+
+// => Using ToString for formatting 
+string formatedString = someNumber.ToString("#,##0");
+Console.WriteLine($"this is formatedString {formatedString}");
+
+
+#endregion
 
 #region String Methods
 string ourString = "   We are learning C#. It is FUN and EASY. Okay maybe just FUN.    ";
