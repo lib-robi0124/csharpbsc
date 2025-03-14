dc4d1651e723455fe8381ee2e3ff6d5a027e0bff
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-03-14 14:02:28 +0100

    thn

---
+++ class05/qinshift.class05/exercise3/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3.Models
+{
+  
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        public Student(string name, string academy, string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Academy: {Academy}, Group: {Group}");
+        }
+
+    }
+}
---
+++ class05/qinshift.class05/exercise3/Program.cs
@@ -0,0 +1,49 @@
+﻿using exercise3.Models;
+using System.ComponentModel.Design;
+
+#region Students
+
+string continueExecute = string.Empty;
+while (continueExecute.ToLower() != "n")
+{
+    Student[] students = new Student[]
+       {
+            new Student("Toska", "Science Academy", "A1"),
+            new Student("Robert", "Math Academy", "B2"),
+            new Student("Sara", "Engineering Academy", "C3"),
+            new Student("Zivkica", "Arts Academy", "D4"),
+            new Student("Ada", "Business Academy", "E5")
+       };
+   
+
+    Console.WriteLine("Please write a Student name:");
+    string studentName = Console.ReadLine();
+
+    bool found = false;
+    for (int i = 0; i < students.Length; i++)
+    {
+        if (students[i].Name == studentName)
+        {
+            students[i].DisplayInfo();
+            found = true;
+            break;
+        }
+    }
+    if (!found)
+    {
+        Console.WriteLine("Student not found");
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine($"Students in a list are: {students[i].Name}");
+        }
+    }
+    
+    Console.WriteLine("Do you want to continue? Y / N");
+    continueExecute = Console.ReadLine();
+    Console.Clear();
+}
+
+
+    #endregion
+
+    Console.ReadLine();
