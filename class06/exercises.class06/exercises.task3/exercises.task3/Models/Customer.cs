942cb3f8bcdda712adcc1e4bf5da61429bf72ec6
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-03-16 19:48:01 +0100

    wsde

---
+++ class06/exercises.class06/exercises.task3/exercises.task3/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿namespace exercises.task3.Models
+{
+    public class Customer
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Card { get; set; }
+        public int Pin { get; set; }
+        public int Balance { get; set; }
+        public Customer(string fName, string lName, string card, int pin, int balace)
+
+        {
+            FName = fName;
+            LName = lName;
+            Card = card;
+            Pin = pin;
+            Balance = balace;
+        }
+        public void CardBalance()
+        {
+            Console.WriteLine($"The current balance from you're account is {Balance}");
+        }
+        public void CashWithdrawal(string withdraw)
+        {
+            if (int.TryParse(withdraw, out int userwithdraw))
+            {
+                if (userwithdraw < Balance)
+                {
+                    Balance -= userwithdraw;
+                    return;
+                }
+                Console.WriteLine("You do not have enough money to withdraw");
+            }
+        }
+        public void CashDeposit(string deposit)
+        {
+            if (int.TryParse(deposit, out int userDeposit))
+            {
+                Balance += userDeposit;
+                return;
+            }
+        }
+}
---
+++ class06/exercises.class06/exercises.task3/exercises.task3/Program.cs
@@ -0,0 +1,55 @@
+﻿using exercises.task3.Models;
+using System.Globalization;
+
+
+#region ATM.class06.Task3
+Customer bob = new Customer("Bob", "Bobsky", "1234-1234-1234-1234", 4325, 650);
+
+
+Console.WriteLine("Welcome to the ATM app:");
+Console.WriteLine("Please enter your card number:");
+string card = Console.ReadLine();
+if (card != bob.Card)
+{
+    Console.WriteLine("Invalid card number. Exiting...");
+    return;
+}
+Console.WriteLine("Please enter your pin:");
+int pin = int.Parse(Console.ReadLine()); //must enter 4 digit pin
+if (pin != bob.Pin)
+{
+    Console.WriteLine("Invalid pin. Exit or enter again...");
+    return;
+}
+Console.WriteLine("Please make a chice from 1 to 4");
+Console.WriteLine("1. Balance checking");
+Console.WriteLine("2. Cash withdrawal");
+Console.WriteLine("3. Cash deposition");
+Console.WriteLine("4. Exit");
+string choice = Console.ReadLine();
+
+switch (choice)
+{
+    case "1":
+        bob.CardBalance();
+
+        break;
+    case "2":
+        Console.WriteLine("How much would you like to withdraw?");
+        string withdraw = Console.ReadLine();
+        bob.CashWithdrawal(withdraw);
+        break;
+    case "3":
+        Console.WriteLine("How much would you like to deposit?");
+        string deposit = Console.ReadLine();
+        bob.CashDeposit(deposit);
+        break;
+    case "4":
+        return;
+    default:
+        Console.WriteLine("Invalid choice. Try again.");
+        break;
+}
+
+
+#endregion
