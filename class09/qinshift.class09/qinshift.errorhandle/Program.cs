183b39b0875a65afec1f2b648bae9187a0a4d97b
lib-robi0124 <robert.ristovski@gmail.com>
2025-03-26 19:05:50 +0100

    ok

---
+++ class09/qinshift.class09/qinshift.errorhandle/Program.cs
@@ -0,0 +1,94 @@
+﻿
+
+
+#region Exceptions
+
+Console.WriteLine(" enter a number:");
+
+try
+{
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"the number you entered is number: {number}");
+}
+catch (Exception ex)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(" wrong input , try again to enter a valid number");
+    //Console.WriteLine(ex.ToString());
+    Console.WriteLine($" ex msg {ex.Message}");
+}
+finally
+{ 
+    Console.ResetColor();
+    Console.WriteLine(" number example finished");
+    Console.ReadLine();
+}
+
+#endregion
+
+
+#region Self throw ex
+
+Console.WriteLine(" == self throw ex ==");
+
+Console.WriteLine(" Enter a letter A or B:");
+
+try
+{
+    string letter = Console.ReadLine().ToLower().Trim();
+    if (letter == "a" || letter == "b")
+    {
+        Console.WriteLine(" super, you have entered A or B");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        throw new Exception(" That was not letter A or B!");
+    }
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+    //Console.WriteLine(ex.InnerException.Message);
+
+}
+finally
+{
+    Console.ResetColor();
+    Console.WriteLine("pres any key to exit");
+    Console.ReadLine();
+}
+
+
+#endregion
+
+#region Handle spec ex
+try
+{
+    Console.WriteLine(" enter some character!");
+    char char1 = char.Parse(Console.ReadLine());
+    Console.WriteLine($" you entered the character {char1}");
+
+    int[] numbers = { 1, 2 };
+    numbers[2] = 3;
+}
+catch (FormatException ex)
+{
+
+    Console.WriteLine(" enter non charater");
+}
+catch(IndexOutOfRangeException ex)
+{
+    Console.WriteLine(" Array out of range..");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
+#endregion
+
+Console.ReadLine();
